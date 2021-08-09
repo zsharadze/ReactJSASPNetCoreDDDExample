e4f6d80ea1ea72ddcc59e1a5b8a5d30a7a118ad3
zsharadze9@gmail.com <zsharadze9@gmail.com>
2021-08-10 02:44:31 +0400

    removed extra comment

--- react-app/src/data/datastore.js
+++ react-app/src/data/datastore.js
@@ -4,9 +4,5 @@ import { bookReducer } from "../reducers/BookReducer";
 import { asyncActions } from "./AsyncMiddleware";
 import { CommonReducer } from "../reducers/CommonReducer";
 
-//export const dataStore 
-  //  = createStore(publisherReducer, applyMiddleware(asyncActions));
-
-
-    export const dataStore
- = createStore(CommonReducer(publisherReducer, bookReducer), applyMiddleware(asyncActions));
+export const dataStore
+  = createStore(CommonReducer(publisherReducer, bookReducer), applyMiddleware(asyncActions));
